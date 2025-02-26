6bc1b58f75b2d604511efc0e4717975811ca7625
aceday <markcediebuday@gmail.com>
2025-02-26 16:30:55 +0800

    Move page to designed folder

---
+++ QR-Line/asset/js/base.js
@@ -0,0 +1,4 @@
+
+function gotoPage(page) {
+    window.location.href = page;
+}
--- QR-Line/includes/navbar.php
+++ QR-Line/includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
     <div class="container d-flex justify-content-center align-items-center">
         <span class="navbar-brand mb-0 text-white d-flex align-items-center">
-            <img src="asset/images/logo.png" alt="PSU logo" width="40" height="40" class="me-2">
+            <img src="./../asset/images/logo.png" alt="PSU logo" width="40" height="40" class="me-2">
             <span class="fs-5 fw-normal">QR-Line: Palawan State University</span>
         </span>
     </div>
