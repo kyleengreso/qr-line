7bcd726c741796aac7c0273b9a4700ef76f90e6b
aceday <markcediebuday@gmail.com>
2025-03-10 21:59:44 +0800

    Remove temporary protection

--- QR-Line/base.php
+++ QR-Line/base.php
@@ -10,36 +10,36 @@ $employee_path = $root_path . "employee/";
 $api_path = $root_path . "api\/";
 
 function login_as_employee() {
-    global $auth_path, $admin_path, $employee_path;
-    if (isset($_SESSION['username']) && isset($_SESSION['role_type'])) {
-        if ($_SESSION['role_type'] == "admin" && basename($_SERVER['PHP_SELF']) != "dashboard.php") {
-            header("Location: " . $admin_path . "dashboard.php");
-            exit();
-        } else if ($_SESSION['role_type'] == "employee" && basename($_SERVER['PHP_SELF']) != "counter.php") {
-            header("Location: " . $employee_path . "counter.php");
-            exit();
-        }
-    } else if (basename($_SERVER['PHP_SELF']) != "login.php") {
-        header("Location: " . $auth_path . "login.php");
-        exit();
-    }
+    // global $auth_path, $admin_path, $employee_path;
+    // if (isset($_SESSION['username']) && isset($_SESSION['role_type'])) {
+    //     if ($_SESSION['role_type'] == "admin" && basename($_SERVER['PHP_SELF']) != "dashboard.php") {
+    //         header("Location: " . $admin_path . "dashboard.php");
+    //         exit();
+    //     } else if ($_SESSION['role_type'] == "employee" && basename($_SERVER['PHP_SELF']) != "counter.php") {
+    //         header("Location: " . $employee_path . "counter.php");
+    //         exit();
+    //     }
+    // } else if (basename($_SERVER['PHP_SELF']) != "login.php") {
+    //     header("Location: " . $auth_path . "login.php");
+    //     exit();
+    // }
 }
 
-function restrict_admin_access() {
-    global $auth_path;
-    if (!isset($_SESSION['username']) || $_SESSION['role_type'] != "admin") {
-        header("Location: " . $auth_path . "login.php");
-        exit();
-    }
-}
+// function restrict_admin_access() {
+//     global $auth_path;
+//     if (!isset($_SESSION['username']) || $_SESSION['role_type'] != "admin") {
+//         header("Location: " . $auth_path . "login.php");
+//         exit();
+//     }
+// }
 
-function restrict_employee_access() {
-    global $auth_path;
-    if (!isset($_SESSION['username']) || $_SESSION['role_type'] != "employee") {
-        header("Location: " . $auth_path . "login.php");
-        exit();
-    }
-}
+// function restrict_employee_access() {
+//     global $auth_path;
+//     if (!isset($_SESSION['username']) || $_SESSION['role_type'] != "employee") {
+//         header("Location: " . $auth_path . "login.php");
+//         exit();
+//     }
+// }
 
 
 function after_js() {
