c528749fdb638d989eb40aaccbcb35f73c173233
aceday <markcediebuday@gmail.com>
2025-05-21 11:19:49 +0800

    RM PR

--- public/includes/db_conn.php
+++ public/includes/db_conn.php
@@ -13,23 +13,10 @@ $port = 3306;
 // $password = "QRlin3rs*";
 // $dbname = "u273960544_qrline";
 
-// PROD DB 2
-    // $servername = "qrliners-qrline.k.aivencloud.com";
-    // $username = "prod";
-    // $password = "AVNS_3m8N9PgsxvAYmyKK6sh";
-    // $dbname = "qr_queue_system";
-    // $port = 22410;
-    // $ca_cert = __DIR__ . "/ca.pem";
-// $ssl_options = [
-//     MYSQLI_OPT_SSL_VERIFY_SERVER_CERT => true,
-//     MYSQLI_OPT_SSL_CA => $ca_cert,
-// ];
 
 try {
     // Create a new MySQLi instance with SSL options
     $conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-    // $conn->ssl_set(null, null, $ca_cert, null, null);
 
     if (!$conn->real_connect($servername, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL)) {
         die("Connection failed: " . $conn->connect_error);
